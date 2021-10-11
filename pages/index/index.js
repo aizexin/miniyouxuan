fbdf2e23d11d89f8b9c3e03c77097159f735646d
aizexin <443792431@qq.com>
2021-10-11 16:45:57 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,43 @@
+import {
+  request
+} from "../../request/index.js"
+Page({
+  data: {
+    swiperList: [],
+    categoryList:[]
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+   this.getswiperList()
+   this.getCategoryList()
+  },
+
+  // net work
+  // 获取轮播图数据
+  getswiperList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+    })
+    .then(result => {
+      this.setData({
+        swiperList: result.data.message
+      })
+    })
+  },
+  // 获取分类导航数据
+  getCategoryList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+    })
+    .then(result => {
+      this.setData({
+        categoryList: result.data.message
+      })
+    })
+  }
+})
