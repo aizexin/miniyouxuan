2f83b59fc458a354a41f119c583c72e3c077a41d
aizexin <443792431@qq.com>
2021-10-12 10:57:09 +0800

    分类左侧菜单展示

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,42 @@
-// pages/category/index.js
+import {
+  request
+} from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧菜单数据
+    leftMenuList:[],
+    // 右侧数据
+    rightContent:[],
+    categoryList:[],
+    // 被电击的菜单
+    currentIndex:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCategoryData()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // net work
+  getCategoryData() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories'
+    })
+    .then(result => {
+      this.categoryList = result.data.message
+      // 左侧大菜单数据
+      let leftMenuList = this.categoryList.map(v=>v.cat_name)
+      // 右侧商品数据
+      let rightContent = this.categoryList[0].children
+      this.setData({
+        leftMenuList: leftMenuList,
+        rightContent: rightContent
+      })
+    })
   }
 })
